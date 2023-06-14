ff59c228f6cc637e1d805c5281d7b6d9cdc96499
Nicholas Lee <nlee@nicholass-mbp.mynetworksettings.com>
2023-06-14 19:08:40 -0400

    working on current weather

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,20 @@ import "jquery"
 import "jquery_ujs"
 import "popper"
 import "bootstrap"
+
+$(document).ready(function() {
+  var $searchForm = $("#address-form");
+  $searchForm.submit(function(e) {
+    e.preventDefault();
+
+    var address = $("#address").val();
+    $.get("/api/forecast", { address: address }).done(function(data) {
+      console.log(data);
+      $("#location").text(data.address);
+      $("#updated-at").text(data.updated_at);
+      $("#current-temp").text(data.current.temperature + " " + data.current.temperatureUnit);
+    }).error(function() {
+      alert("Address not found");
+    });
+  });
+})
