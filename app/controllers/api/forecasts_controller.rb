c65cece2c32922020f83f94cae943ac8154f8017
Nicholas Lee <nlee@Nicholass-MBP.home>
2023-06-14 08:41:08 -0400

    add endpoint

---
+++ app/controllers/api/forecasts_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  class ForecastsController < ApplicationController
+    def show
+      render json: {}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    get 'forecast', to: 'forecasts#show'
+  end
 end
