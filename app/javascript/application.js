9492b3947e03fcf73ea395f4f14f05d8277b5b37
Nicholas Lee <nlee@nicholass-mbp.mynetworksettings.com>
2023-06-15 11:50:04 -0400

    ugly forecast

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,18 +7,35 @@ import "popper"
 import "bootstrap"
 
 $(document).ready(function() {
-  var $searchForm = $("#address-form");
+  const $searchForm = $("#address-form");
+  let forecastPeriod = function(data) {
+    const pdNameHtml = '<span class="pd-name">' + data.name + '</span';
+    const picHtml = '<img class="pd-pic" src="' + data.icon + '" />';
+    const tempHtml = '<span class="pd-temp">' + data.temperature + ' ' + data.temperatureUnit + '</span>';
+    return '<div class="forecast-pd col-sm-3">' +  pdNameHtml + '<br>' + picHtml + '<br>' + tempHtml + '</div>';
+  };
   $searchForm.submit(function(e) {
     e.preventDefault();
 
-    var address = $("#address").val();
+    const address = $("#address").val();
     $.get("/api/forecast", { address: address }).done(function(data) {
       console.log(data);
+      $("#address").val('');
       $("#location").text(data.address);
-      $("#updated-at").text(data.updated_at);
+      $("#updated-at").text(new Date(data.updated_at));
       $("#current-temp").text(data.current.temperature + " " + data.current.temperatureUnit);
+      $("#short-forecast").text(data.current.shortForecast);
+      $("#rain-chance").text(data.current.probabilityOfPrecipitation.value + "%");
+      $("#wind").text(data.current.windSpeed + " " + data.current.windDirection);
+
+      const firstTwelve = data.forecast.slice(0, 12);
+      firstTwelve.forEach((forecast) => {
+        let fc = forecastPeriod(forecast);
+        $("#forecast-holder").append(fc);
+      });
     }).error(function() {
       alert("Address not found");
     });
   });
 })
+import * as bootstrap from "bootstrap"
