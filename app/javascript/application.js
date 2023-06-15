15644c33a53d0c86e4b50e01c8622ad744abe413
Nicholas Lee <nlee@Nicholass-MacBook-Pro.local>
2023-06-15 12:46:20 -0400

    display forecast

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,38 +4,44 @@ import "controllers"
 import "jquery"
 import "jquery_ujs"
 import "popper"
-import "bootstrap"
 
 $(document).ready(function() {
   const $searchForm = $("#address-form");
   let forecastPeriod = function(data) {
-    const pdNameHtml = '<span class="pd-name">' + data.name + '</span';
+    const date = new Date(data.startTime);
+    const pdNameHtml = '<span class="pd-name">' + data.name + '</span>';
+    const dateHtml = '<span class="pd-date">' + date.getMonth() + '/' + date.getDate() + '</span>';
     const picHtml = '<img class="pd-pic" src="' + data.icon + '" />';
-    const tempHtml = '<span class="pd-temp">' + data.temperature + ' ' + data.temperatureUnit + '</span>';
-    return '<div class="forecast-pd col-sm-3">' +  pdNameHtml + '<br>' + picHtml + '<br>' + tempHtml + '</div>';
+    const tempHtml = '<span class="pd-temp">' + data.temperature + '&deg; ' + data.temperatureUnit + '</span>';
+    return '<div class="forecast-pd">' +  pdNameHtml + '<br>' + dateHtml + '<br>' + picHtml + '<br>' + tempHtml + '</div>';
   };
   $searchForm.submit(function(e) {
     e.preventDefault();
 
     const address = $("#address").val();
     $.get("/api/forecast", { address: address }).done(function(data) {
-      console.log(data);
-      $("#address").val('');
-      $("#location").text(data.address);
-      $("#updated-at").text(new Date(data.updated_at));
-      $("#current-temp").text(data.current.temperature + " " + data.current.temperatureUnit);
-      $("#short-forecast").text(data.current.shortForecast);
-      $("#rain-chance").text(data.current.probabilityOfPrecipitation.value + "%");
-      $("#wind").text(data.current.windSpeed + " " + data.current.windDirection);
+      if (data) {
+        $("#forecast-holder").empty();
+        $("#address").val('');
+        $("#location").text(data.address);
+        $("#updated-at").text(new Date(data.updated_at).toLocaleString());
+        $("#current-temp").text(data.current.temperature + '\xB0 ' + data.current.temperatureUnit);
+        $("#short-forecast").text(data.current.shortForecast);
+        $("#rain-chance").text(data.current.probabilityOfPrecipitation.value + "%");
+        $("#wind").text(data.current.windSpeed + " " + data.current.windDirection);
 
-      const firstTwelve = data.forecast.slice(0, 12);
-      firstTwelve.forEach((forecast) => {
-        let fc = forecastPeriod(forecast);
-        $("#forecast-holder").append(fc);
-      });
+        const firstTwelve = data.forecast.slice(0, 12);
+        firstTwelve.forEach((forecast) => {
+          let fc = forecastPeriod(forecast);
+          $("#forecast-holder").append(fc);
+        });
+      } else {
+        alert("Address not found");
+      }
     }).error(function() {
       alert("Address not found");
+    }).always(function() {
+      $($("#address-form").find('input')[1]).prop('disabled', false)
     });
   });
 })
-import * as bootstrap from "bootstrap"
